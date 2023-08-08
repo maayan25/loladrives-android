b9b0f939c59045ce09ba4fed8b9263c4c8d3d784
Ma'ayan Armony <maayan.armony@kcl.ac.uk>
2023-08-08 08:53:44 +0100

    Add somments to test

--- app/src/test/java/org/rdeapp/pcdftester/Sinks/VelocityProfileTest.kt
+++ app/src/test/java/org/rdeapp/pcdftester/Sinks/VelocityProfileTest.kt
@@ -17,6 +17,7 @@ class VelocityProfileTest {
 
     /**
      * Test if the velocity profile is updated correctly when the speed is 0 km/h.
+     * The stopping time should be updated and the other counters should be 0.
      */
     @Test
     fun updateVelocityProfileWithStop() {
@@ -32,6 +33,7 @@ class VelocityProfileTest {
 
     /**
      * Test if the velocity profile is updated correctly when the speed is 10 km/h.
+     * None of the counters should be updated.
      */
     @Test
     fun updateVelocityProfileWithSlowSpeed() {
@@ -44,11 +46,14 @@ class VelocityProfileTest {
         assertTrue(0.0 == velocityProfile.getVeryHighSpeed())
     }
 
+    /**
+     * Test that the stopping time is calculated correctly when the speed is 0 km/h.
+     */
     @Test
     fun getStoppingTime() {
         val speed = 0.0
 
-        // update the velocity profile with a speed of 0 km/h
+        // Update the velocity profile with a speed of 0 km/h
         velocityProfile.updateVelocityProfile(speed)
         val firstIteration = velocityProfile.getTimeDifference()
         assertTrue(firstIteration / 60000 == velocityProfile.getStoppingTime())
@@ -59,6 +64,9 @@ class VelocityProfileTest {
         assertTrue((firstIteration + secondIteration) / 60000 == velocityProfile.getStoppingTime())
     }
 
+    /**
+     * Test that the high-speed time is calculated correctly when the speed is 101 km/h.
+     */
     @Test
     fun getHighSpeed() {
         val speed = 101.0
@@ -73,11 +81,15 @@ class VelocityProfileTest {
         assertTrue(secondIteration / 60000 == velocityProfile.getHighSpeed())
     }
 
+    /**
+     * Test that the very high-speed time is calculated correctly when the speed is 151 km/h.
+     * The high-speed time should also be increased.
+     */
     @Test
     fun getVeryHighSpeed() {
         val speed = 151.0
 
-        // update the velocity profile with a speed of 151 km/h
+        // Update the velocity profile with a speed of 151 km/h
         velocityProfile.updateVelocityProfile(speed)
         val firstIteration = velocityProfile.getTimeDifference()
 
@@ -86,7 +98,7 @@ class VelocityProfileTest {
         val secondIteration = velocityProfile.getTimeDifference()
         assertTrue(secondIteration / 60000 == velocityProfile.getVeryHighSpeed())
 
-        // the high-speed time should also increase
+        // The high-speed time should also increase
         assertTrue(secondIteration / 60000 == velocityProfile.getHighSpeed())
     }
 }
