9bf8110c1e1cfe1d151ea7d3b8d571dcb22ce6b1
melane6 <mnavaratnarajah6@gmail.com>
2023-10-16 21:52:23 +0100

    Remove total distance

--- app/src/main/java/org/rdeapp/pcdftester/Sinks/RDEUIUpdater.kt
+++ app/src/main/java/org/rdeapp/pcdftester/Sinks/RDEUIUpdater.kt
@@ -63,7 +63,6 @@ class RDEUIUpdater(
 
                 // Check progress (urban[1], rural[2], motorway[3])
                 fragment.trajectoryAnalyser.updateProgress(
-                    outputs[0],
                     outputs[1],
                     outputs[2],
                     outputs[3],
